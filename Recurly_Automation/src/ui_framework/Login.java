ac059a3bc3475602ea43d0af0163a488a98365e8
J. Drill <JJDrill@hotmail.com>
2017-01-11 21:02:44 -0700

    Login initial checkin

---
+++ Recurly_Automation/src/ui_framework/Login.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui_framework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ *
+ * @author J. Drill
+ */
+public class Login {
+    WebDriver driver = null;
+    
+    // Page Objects
+    By byUserName = By.id("user_email");
+    By byPassword = By.id("user_password");
+    By byLogIn = By.id("submit_button");
+    
+    // Constructor
+    public Login(WebDriver driver){
+        this.driver = driver;
+    }
+    
+    // Set the user name
+    public void setUserName(String userName){
+        driver.findElement(byUserName).sendKeys(userName);
+    }
+    
+    // Set the password
+    public void setPassword(String password){
+        driver.findElement(byPassword).sendKeys(password);
+    }
+    
+    // Click the login button
+    public void clickLogin(){
+        driver.findElement(byLogIn).click();
+    }
+    
+    // Shortcut function to login a user
+    public void loginUser(String userName, String password){
+        this.setUserName(userName);
+        this.setPassword(password);
+        this.clickLogin();
+    }
+}
